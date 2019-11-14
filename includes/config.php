d660499e2c5d935799d0be72b82cd1ff539abb7d
chedia363 <57214047+chedia363@users.noreply.github.com>
2019-11-14 10:43:56 +0100

    update config and git

---
+++ includes/config.php
@@ -0,0 +1,10 @@
+<?php  
+    define("DB_NAME", "quran_waqf");
+    define("DB_USER", "root");
+    define("DB_PASSWORD", "chedia123");
+    define("DB_HOST", "localhost");
+    define("SYS_URL", "http://aynettree.test");
+    define("DEBUGGING", false);
+    define("DEFAULT_LOCALE", "en_us");
+    define("LICENCE_KEY", "");
+    ?>
